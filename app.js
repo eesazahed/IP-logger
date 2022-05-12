fb515bd16e9686abfb34a09c6421ac3580471d66
Eesa Zahed <eesa.zahed@gmail.com>
2022-05-12 18:28:09 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+const { MongoClient } = require("mongodb");
+
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  const url =
+    "mongodb+srv://NOT_MY_USERNAME:NOT_MY_PASSWORD@NOT_MY_DB.knkja.mongodb.net/DB?retryWrites=true&w=majority";
+  const client = new MongoClient(url);
+
+  await client.connect();
+
+  const col = client.db("DB").collection("logs");
+  const findResult = await col.find({}).toArray();
+
+  findResult.sort((a, b) => parseFloat(b.timestamp) - parseFloat(a.timestamp));
+
+  await client.close();
+
+  res.render(path.join(__dirname, "/index.html"), { logs: findResult });
+});
+
+app.listen(port, (req, res) =>
+  console.log(`Running on http://localhost:${port}`)
+);
