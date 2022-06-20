368bda9254e76e92104acbba496a76aa7ea06dee
Eesa Zahed <eesa.zahed@gmail.com>
2022-06-20 11:49:32 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -11,11 +11,14 @@ const port = process.env.PORT || 5000;
 app.use(async (req, res) => {
   const url =
     "mongodb+srv://NOT_MY_USERNAME:NOT_MY_PASSWORD@NOT_MY_DB.knkja.mongodb.net/DB?retryWrites=true&w=majority";
-  const client = new MongoClient(url);
+  const client = new MongoClient(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
   await client.connect();
 
-  const col = client.db("DB").collection("logs");
+  const col = client.db("IP_LOGGER").collection("logs");
   const findResult = await col.find({}).toArray();
 
   findResult.sort((a, b) => parseFloat(b.timestamp) - parseFloat(a.timestamp));
