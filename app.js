f743c169d66bf21bcc7c30ef6fda7a7d636a488d
Eesa Zahed <eesa.zahed@gmail.com>
2022-06-20 11:35:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,28 @@
 require("dotenv").config();
 
+const app = require("express")();
+
+app.set("view engine", "ejs");
+
 const { MongoClient } = require("mongodb");
 
-const express = require("express");
-const path = require("path");
-
-const app = express();
 const port = process.env.PORT || 5000;
 
-app.engine("html", require("ejs").renderFile);
-app.set("view engine", "html");
-
-app.get("/", async (req, res) => {
+app.use(async (req, res) => {
   const url =
     "mongodb+srv://NOT_MY_USERNAME:NOT_MY_PASSWORD@NOT_MY_DB.knkja.mongodb.net/DB?retryWrites=true&w=majority";
   const client = new MongoClient(url);
 
   await client.connect();
 
-  const col = client.db("DB").collection("logs");
+  const col = client.db("IP_LOGGER").collection("logs");
   const findResult = await col.find({}).toArray();
 
   findResult.sort((a, b) => parseFloat(b.timestamp) - parseFloat(a.timestamp));
 
   await client.close();
 
-  res.render(path.join(__dirname, "/index.html"), { logs: findResult });
+  res.render("index", { logs: findResult });
 });
 
-app.listen(port, (req, res) =>
-  console.log(`Running on http://localhost:${port}`)
-);
+app.listen(port);
